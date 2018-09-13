3dbef455c0ea2dec25eac8a582c6afb9e618e0ca
Damis Garcia <damisgarcia@gmail.com>
2018-09-13 01:45:59 -0300

    Create Controller Users

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,40 +1,9 @@
-const path = require("path");
-const faker = require('faker');
-
-const Const = require('./index.const');
+const UsersController = require('../controllers/users');
 
 const Router = (app) => {
-    app.get('/user', (req, res) => {
-        let data = createUser();
-
-        res.status(Const.Status.OK).send(data);
-    });
-
-    app.get('/users/:num', (req, res) => {
-        var users = [];
-        var num = req.params.num;
-
-        if (isFinite(num) && num > 0) {
-            for (i = 0; i <= num - 1; i++) {
-                users.push(createUser());
-            }
-
-            res.status(Const.Status.OK).send(users);
-
-        } else {
-            res.status(Const.Status.FAIL).send({ message: 'Número informar é inválido' });
-        }
-    });
+    UsersController(app);
 };
 
-function createUser(){
-    return {
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-        username: faker.internet.userName(),
-        email: faker.internet.email(),
-        avatarUrl: faker.image.avatar()
-    };
-}
+
 
 module.exports = Router;
